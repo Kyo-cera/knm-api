356b16897dfebc6ce38ca079204b99caf2305a33
KyoFred <freddy.herrera@dit.piksel.it>
2024-10-18 13:47:29 +0200

    Middleware per servire file statici dalla cartella 'src/attachments'

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
 import express, { Request, Response } from 'express';
 import routes from './routes/routes';
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware per il parsing del JSON
 app.use(express.json());
 
-app.get('/', async (req: Request, res: Response)=>{
+// Middleware per servire file statici dalla cartella 'src/attachments'
+app.use('/attachment', express.static(path.join(__dirname, 'attachment'))); // Modifica qui
+
+app.get('/', async (req: Request, res: Response) => {
     res.send(`Hi KNM Api`);
 });
 
 app.use('/', routes);
 
-app.listen(PORT, ()=>{
-    console.log(`App KNM è online nella porta on port: ${process.env.PORT}`);
-}); 
+app.listen(PORT, () => {
+    console.log(`App KNM è online nella porta: ${PORT} - ${__dirname}`);
+});
